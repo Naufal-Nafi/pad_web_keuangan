536679d6a9c408c3d173be12324d40ec7fde0c33
Naufal-Nafi <naufalabyu4869@gmail.com>
2025-06-16 18:59:06 +0700

    railway 3

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.js", "resources/css/app.css"],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
     ],
     build: {
-        manifest: true,
-        emptyOutDir: true,
-        outDir: "public/build", // Specify the output directory for built files
-    },
-    publicDir: false, // penting agar tidak bentrok dengan public Laravel
+        sourcemap: true, // Enable source maps for easier debugging
+        outDir: 'public/build', // Specify the output directory for built files
+    }
 });
